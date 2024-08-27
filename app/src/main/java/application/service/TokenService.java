0fdf8f2b9de52f0a2c2bbbd97b16e3e2858ce1f2
DavidMarques <davidmarqqs@gmail.com>
2024-08-27 19:38:16 +0000

    feat: security config

--- app/src/main/java/application/service/TokenService.java
+++ app/src/main/java/application/service/TokenService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import application.model.Usuario;
 
