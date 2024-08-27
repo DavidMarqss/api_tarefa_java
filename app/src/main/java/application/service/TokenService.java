9cec219d33d3b4147cf61bacee2885c6dc596ebd
DavidMarques <davidmarqqs@gmail.com>
2024-08-27 17:57:45 +0000

    feat: tokenservice

--- app/src/main/java/application/service/TokenService.java
+++ app/src/main/java/application/service/TokenService.java
@@ -2,6 +2,7 @@ package application.service;
 
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
@@ -14,6 +15,10 @@ public class TokenService {
     public String generateToken(Usuario usuario) {
         try{
             Algorithm algorithm = Algorithm.HMAC256(secreto);
+            return JWT.create()
+                .withIssuer("API Tarefas")
+                .withSubject(usuario.getNomeUsuario())
+                .sign(algorithm);
         }catch(JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar JWT", exception);
         }
